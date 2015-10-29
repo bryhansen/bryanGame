c4adda66a6b16f8ea5d64fe51e7c953f4e6d4c84
Bryan Hansen <bry.d.hansen@gmail.com>
2015-10-29 15:50:50 -0600

    Initial user interface

---
+++ BryanGame/BryanGame/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BryanGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button anotherButton = new Button();
+            anotherButton.Tag = new Point(1, 4);
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            fettPicture.Show();
+        }
+
+        private void hideButton_Click(object sender, EventArgs e)
+        {
+            fettPicture.Hide();
+        
+        }
+
+        private void fettPicture_MouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox picture = sender as PictureBox;
+
+            Console.WriteLine("This is a test");
+
+        }
+    }
+}
